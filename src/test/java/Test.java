340caaa0ebdf5b187e699aae49d5c71ad2e890ef
Dave Syer <dsyer@pivotal.io>
2018-01-15 14:48:48 +0000

    Add notes on AOT

---
+++ src/test/java/Test.java
@@ -0,0 +1,8 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+public class Test {
+    private static Log logger = LogFactory.getLog(Test.class);
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
